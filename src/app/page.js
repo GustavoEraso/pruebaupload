736f0d3e48d130f85e2849be5581eded14c83ed8
Gustavo Eraso <gustavoeraso1988@gmail.com>
2024-03-09 18:51:46 -0300

    prueba ssh

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import UploadImage from "./components/UploadImage";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>hola como tas?</h1>
       <UploadImage/>
     </main>
   );
